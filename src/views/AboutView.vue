298e758117d50885df92d07cb9730c6ae114b45c
Thanadol041 <88518833+Thanadol041@users.noreply.github.com>
2025-03-25 19:36:23 +0700

    Update AboutView.vue
    เพิ่มรายละเอียดหน้าลงทะเบียนสินค้าใหม่

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,79 @@
 <template>
   <div class="Main">
-    <img :src="imageUrl" alt="Description" class="Brand"/>
     <div class="Register">
+      <img :src="imageUrl" alt="Description" class="Brand"/>
       <h2 class="ordertitle">ลงทะเบียนสินค้า</h2>
-      <label style="margin-left: -1250px;">ชื่อสินค้า</label><br>
-      <input type="text" id="productname" v-bind:style="{ fontSize: '20px', paddingRight: '450px' }"><br><br>
+      <div class="nameprod">
+        <label style="margin-left: -1060px;">ชื่อสินค้า</label><br>
+        <input type="text" id="productname" v-bind:style="{ fontSize: '20px', paddingRight: '250px' }"><br><br>
+      </div>
 
-      <label style="margin-left: -1205px;">Beacon Name</label><br>
-      <input type="text" id="bname" v-bind:style="{ fontSize: '20px', paddingRight: '450px' }"><br><br><br>
+      <div class="prodid">
+        <label style="margin-left: -1045px;">รหัสสินค้า</label><br>
+        <input type="text" id="bname" v-bind:style="{ fontSize: '20px', paddingRight: '250px' }"><br><br><br>
+      </div>
 
-      <label style="margin-left: -600px;">ประเภทของสินค้า</label>
-      <input type="radio" id="option1" value="option1" v-model="selectedOption">
-      <label for="option1" style="margin-left: 10px;">แตกง่าย</label>
-      <input type="radio" id="option2" value="option2" v-model="selectedOption">
-      <label for="option2" style="margin-left: 10px;">ไม่แตกง่าย</label><br><br><br>
+      <div class="typeprod">
+        <label style="margin-left: -1850px;">หมวดหมู่ของสินค้า</label>
+        <select style="margin-left: 150px;">
+          <option>--ประเภทของสินค้า--</option>
+          <option>อาหาร</option>
+          <option>อุปกรณ์อิเล็กทรอนิกส์</option>
+          <option>ของใช้ทั่วไป</option>
+        </select>
+      </div><br><br>
 
-      <label style="margin-left: -600px; margin-right: 250px;">น้ำหนักของสินค้า</label>
-      <input type="text" id="weight" v-bind:style="{ fontSize: '16px', width: '30px' }">
-      <label style="margin-left: 20px;">KG</label><br><br><br>
+      <div class="beaconlaunch">
+        <label style="margin-left: -1890px;">Beacon ที่ต้องการติดตั้ง</label>
+        <select style="margin-left: 110px;">
+          <option>--ประเภทของบีคอน--</option>
+          <option>IBKS 105 No 1</option>
+          <option>IBKS 105 No 2</option>
+          <option>IBKS 105 No 3</option>
+          <option>IBKS 105 No 4</option>
+          <option>IBKS 105 No 5</option>
+          <option>IBKS 105 No 6</option>
+        </select>
+      </div><br><br>
 
-      <label style="margin-left: -800px; margin-right: 250px;">เพิ่มรูปภาพ</label>
-      <img :src="importImage" alt="Selected Image" class="Imgprod" @click="triggerFileInput"/>
-      <input type="file" ref="fileInput" @change="onImageSelected" style="display: none" accept="image/*" /><br><br>
+      <div class="locationprod">
+        <label style="margin-left: -750px;">ตำแหน่งจัดเก็บ</label>
+        <input type="text" id="productlocation" v-bind:style="{ fontSize: '20px', width: '100px' }"><br><br>
+      </div>
 
-      <button class="saveProd" @click="saveProdDetail()">SAVE</button> 
+      <div class="prodpieces">
+        <label style="margin-left: -800px;">จำนวน</label>
+        <button @click="decrease">-</button>
+        <span> {{ quantity }} </span>
+        <button @click="increase">+</button><br><br>
+      </div>
+
+      <div class="pictureprod">
+        <label style="margin-left: -800px; margin-right: 250px;">เพิ่มรูปภาพ</label>
+        <img :src="importImage" alt="Selected Image" class="Imgprod" @click="triggerFileInput"/>
+        <input type="file" ref="fileInput" @change="onImageSelected" style="display: none" accept="image/*" /><br><br>
+      </div>
+
+      <input type="checkbox" id="productcheck">
+      <div class="notificationprod">
+        <label style="margin-left: -750px;">แจ้งเตือนเมื่อสินค้าถูกย้าย</label><br><br>
+      </div>
+
+      <div class="roleuser">
+        <label style="margin-left: -650px;">สิทธิการเข้าถึง</label>
+        <select style="margin-left: 50px;">
+          <option>--กำหนดสิทธิ--</option>
+          <option>All</option>
+          <option>Admin</option>
+        </select>
+      </div><br><br>
+
+      <div class="btnmain">
+        <button class="beacon" @click="gotoBeaconView()">บันทึกค่า Beacon ที่นี่!!!</button> 
+        <button class="saveProd" @click="saveProdDetail()">SAVE</button>
+        <button class="logout" @click="logoutSystem()">Log Out</button> 
+      </div>
     </div>
-  </div>
-  <div class="btnmain">
-    <button class="beacon" @click="gotoBeaconView()">บันทึกค่า Beacon ที่นี่!!!</button> 
-    <button class="logout" @click="logoutSystem()">Log Out</button>
   </div>
 </template>
 <style>
@@ -43,13 +88,13 @@
   border: 3px solid black;
   border-radius: 30px;
   display: flexbox;
-  padding-left: 700px;
-  padding-bottom: 150px;
+  padding-left: 1250px;
+  padding-bottom: 180px;
   color: black;
   position: fixed;
-  margin-top: -200px;
+  margin-top: -50px;
   margin-bottom: 10px;
-  margin-left: 600px;
+  margin-left: 60px;
   margin-right: 40px;
   height: 400px;
   width: 150px;
@@ -58,18 +103,25 @@
 .Brand{
   width: 150px;
   height: 150px;
-  margin-right: 900px;
+  margin-top: 50px;
+  margin-left: -2000px;
   border-radius: 50%;
   border: 4px solid black;
 }
 
+.beaconlaunch{
+  margin-left: -65px;
+}
+
 .btnmain{
-  width: 200px;
+  width: 800px;
   display: flex;
   justify-content: left;
   padding: 0 1rem 1.5rem 1rem;
-  flex-direction: column;
-  margin-left: 190px;
+  flex-direction: row;
+  margin-top: 180px;
+  margin-left: -850px;
+  position: fixed;
 }
 
 .Imgprod{
@@ -79,16 +131,64 @@
   border: 1px solid black;
 }
 
+.locationprod{
+  margin-left: -1210px;
+}
+
+.nameprod{
+  margin-left: 100px;
+}
+
+.notificationprod{
+  margin-top: -22px;
+  margin-left: 200px;
+  margin-bottom: -60px;
+}
+
 .ordertitle{
-  margin-left: -660px;
+  margin-left: -600px;
+  margin-top: -200px;
+}
+
+.roleuser{
+  margin-top: 100px;
+  margin-bottom: -160px;
+}
+
+.pictureprod{
+  margin-top: -170px;
+  margin-left: 180px;
+}
+
+.prodid{
+  margin-left: 90px;
+}
+
+.prodpieces{
+  margin-top: 30px;
+  margin-left: -1200px;
+  margin-bottom: -240px;
+}
+
+.typeprod{
+  margin-left: -100px;
 }
 
 #bname{
   margin-left: -700px;
 }
 
+#productcheck{
+  margin-top: 30px;
+  margin-left: -800px;
+}
+
 #productname{
-  margin-left: -700px;
+  margin-left: -710px;
+}
+
+#productlocation{
+  margin-left: 150px;
 }
 
 body{
@@ -104,6 +204,10 @@ button.beacon {
     cursor: pointer;
     margin-top: 20px;
     margin-bottom: 50px;
+    margin-right: 50px;
+    padding-left: 50px;
+    padding-right: 50px;
+
 }
 button.logout {
     background-color: #ff4848;
@@ -112,6 +216,11 @@ button.logout {
     padding: 0.75em 1.5em;
     margin: 0 .5em;
     cursor: pointer;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    margin-right: 50px;
+    padding-left: 50px;
+    padding-right: 50px;
 }
 button.saveProd{
     background-color: #4bff5d;
@@ -122,8 +231,9 @@ button.saveProd{
     cursor: pointer;
     display: flex;
     flex-direction: row;
-    margin-left: -300px;
     margin-top: 20px;
+    margin-bottom: 50px;
+    margin-right: 50px;
 }
 
 </style>
@@ -133,7 +243,8 @@ export default {
     return {
       imageUrl: require('@/assets/duck-logo.webp'),
       selectedOption: null,
-      importImage: require('@/assets/import image.png')
+      importImage: require('@/assets/import image.png'),
+      quantity: 1
     }
   },
   methods: {
@@ -157,6 +268,14 @@ export default {
           this.imageUrl = e.target.result; // ตั้งค่า URL ของภาพที่เลือก
         };
         reader.readAsDataURL(file); // อ่านไฟล์เป็น Base64
+      }
+    },
+    increase() {
+      this.quantity++;
+    },
+    decrease() {
+      if (this.quantity > 1) {
+        this.quantity--;
       }
     }
   }
