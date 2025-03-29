f00f37a295db9267e0fc960dc3ea954a3f426c81
Thanadol041 <88518833+Thanadol041@users.noreply.github.com>
2025-03-29 20:58:54 +0700

    SearchView
    กำลังทำครับ

---
+++ src/views/SearchProduct.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="SearchProduct">
+        <h2 class="titleSearch">Search Product</h2>
+        <div class="main-search-input">
+        <div class="main-search-input-item">
+          <input type="text" placeholder="What are you looking for?" value="" fdprocessedid="ea6hnv">
+        </div>
+        <div class="main-search-input-item location">
+          <div id="autocomplete-container">
+            <input id="autocomplete-input" type="text" placeholder="Location" fdprocessedid="xffmj">
+          </div>
+          <a href="#">
+            <i class="fa fa-map-marker">
+            </i>
+          </a>
+        </div>
+        <div class="main-search-input-item">
+          <select data-placeholder="All Categories" class="chosen-select custom-select" fdprocessedid="d8kc">
+            <option>All Categories</option>
+            <option>อาหาร</option>
+            <option>อุปกรณ์อิเล็กทรอนิกส์</option>
+            <option>ของใช้ทั่วไป</option>
+          </select>
+        </div>
+        <a href="#" class="btn main-search-btn btn-radius btn-lg btn-white">
+          <span class="btn-inner--text no-underline">Search</span>
+        </a>
+    </div>
+    </div><br>
+    <img :src="imageUrl" alt="product in shelves" class="shelves">
+</template>
+
+<style>
+.main-search-input{
+    display: inline-flex;
+    background-color: coral;
+    max-height: inherit;
+    border-radius: 50px;
+    margin-left: 100px;
+    margin-top: 35px;
+    margin-bottom: 35px;
+    padding-top: 10px;
+}
+.main-search-input-item{
+    margin-top: 3px;
+    position: relative;
+    padding-left: 30px;
+    padding-right: 30px;
+    -webkit-box-flex: 1;
+}
+@media (max-width: 992px) {
+    .main-search-input .main-search-input-item {
+        border-radius: 5px;
+        margin-bottom: 1rem;
+    }
+}
+@media (max-width: 992px) {
+    .main-search-input>* {
+        display: inline-block;
+        width: 100%;
+        border: 0;
+        background: hsla(0, 0%, 100%, .1);
+    }
+}
+.main-search-btn {
+    margin-right: .5rem;
+}
+.SearchProduct{
+    display: inline-flex;
+    margin-left: -200px;
+    margin-top: 50px;
+    padding: 2px;
+    position: flex;
+}
+.shelves{
+  width: 1300px;
+  height: 400px;
+  margin-top: 0px;
+  margin-left: 0px;
+  border-radius: 10%;
+  border: 4px solid black;
+}
+.titleSearch{
+    color: navy;
+    font-size: 2rem;
+}
+.btn-lg {
+    font-size: .875rem;
+    line-height: 1.5;
+    padding: .875rem 1rem;
+}
+.btn-radius {
+    border-radius: 25px;
+    padding-left: 2rem;
+    padding-right: 2rem;
+}
+.btn {
+    position: relative;
+    text-transform: uppercase;
+    -webkit-transition: all .15s ease;
+    transition: all .15s ease;
+    will-change: transform;
+    letter-spacing: .025em;
+    font-size: .875rem;
+}
+.btn:hover {
+    box-shadow: 0 7px 14px rgba(50, 50, 93, .1), 0 3px 6px rgba(0, 0, 0, .08);
+    -webkit-transform: translateY(-1px);
+    transform: translateY(-1px);
+}
+
+.btn-white, .btn-white:hover {
+    color: #212529;
+    background-color: #fff;
+    border-color: #fff;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, .11), 0 1px 3px rgba(0, 0, 0, .08);
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+        imageUrl: require('@/assets/shelves1.jpg'),
+    }
+  }
+}
+</script>
