f99b18db790475429c59d944c6dc416a8ad71397
Thanadol041 <88518833+Thanadol041@users.noreply.github.com>
2025-03-29 22:09:13 +0700

    หน้า Signup
    ยังไม่มีอะไรนะครับ แต่ทำเชื่อมกันทุกหน้าไว้แล้ว เดี๋ยวพรุ่งนี้ ผมมีเชียงเม้งนะครับ (ปฏิเสธไปแล้วครับ แต่คนในครอบครัวไม่ให้ขาดครับ) วันนี้อาจจะต้องนอนเร็ว แต่จะรีบกลับมาทำต่อให้เร็วที่สุดนะครับ

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="SignUptemplate">
+        <h2 class="titleSignup">Sign Up</h2>
+    </div>
+</template>
+<style>
+.SignUptemplate{
+    display: inline-flex;
+    margin-left: -1200px;
+    margin-top: 50px;
+    padding: 2px;
+    position: flex;
+}
+.titleSignup{
+    color: rgb(0, 128, 28);
+    font-size: 2rem;
+}
+</style>
+
+<script>
+
+</script>
